506b46ccdf8c614aace45421fad0ac2bea5bbafe
albertbyrone <albertbyrone16777@gmail.com>
2020-10-12 13:05:52 +0300

    create chatgroup admin

--- whatsapp/admin.py
+++ whatsapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import ChatGroup
 # Register your models here.
+class ChatGroupAdmin(admin.ModelAdmin):
+    """ enable group chat admin"""
+
+    list_display = ("id","name","description", "icon", "mute_notifications","icon", )
+    list_filter = ("id","name","description", "icon", "mute_notifications","icon", )
+    list_display_links = ("name", )
+
+admin.site.register(ChatGroup,ChatGroupAdmin)
