631af29937aed17f39499e70096fda5486ddc831
albertbyrone <albertbyrone16777@gmail.com>
2020-10-12 13:29:27 +0300

    configure django channel layers

--- chat/settings.py
+++ chat/settings.py
@@ -71,7 +71,14 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'chat.wsgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channel_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [("127.0.0.1", 6379)],
+        }
+    }
+}
 
 
 # Database
