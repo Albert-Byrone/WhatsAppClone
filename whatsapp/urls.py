36128d38044c3359bfb2cc8635e788342bcea935
albertbyrone <albertbyrone16777@gmail.com>
2020-10-13 08:41:30 +0300

    uncomment

--- whatsapp/urls.py
+++ whatsapp/urls.py
@@ -5,6 +5,6 @@ app_name = 'chat'
 
 urlpatterns = [
     path('',views.index, name='index'),
-    # path('unauthorized/',views.unauthorized, name='unauthorized'),
-    # path('<str: group_id>/',views.room, name='room'),
+    path('unauthorized/',views.unauthorized, name='unauthorized'),
+    path('<str:group_id>/',views.room, name='room'),
 ]
