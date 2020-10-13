ba2d85365eab394755917e3c22177e042541ee56
albertbyrone <albertbyrone16777@gmail.com>
2020-10-13 08:39:56 +0300

    minor changes

--- whatsapp/admin.py
+++ whatsapp/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import ChatGroup
 # Register your models here.
 class ChatGroupAdmin(admin.ModelAdmin):
-    """ enable group chat admin"""
+    """ enable Chart Group admin """
+    list_display = ('id', 'name', 'description', 'icon', 'mute_notifications', 'date_created', 'date_modified')
+    list_filter = ('id', 'name', 'description', 'icon', 'mute_notifications', 'date_created', 'date_modified')
+    list_display_links = ('name',)
 
-    list_display = ("id","name","description", "icon", "mute_notifications","icon", )
-    list_filter = ("id","name","description", "icon", "mute_notifications","icon", )
-    list_display_links = ("name", )
+admin.site.register(ChatGroup, ChatGroupAdmin)
 
-admin.site.register(ChatGroup,ChatGroupAdmin)
--- whatsapp/models.py
+++ whatsapp/models.py
@@ -4,13 +4,13 @@ from django.urls import reverse
 
 
 class ChatGroup(Group):
-    """" Extends the group models to add more information """
-    description  = models.TextField(blank=True,help_text="the description")
-    mute_notifications  = models.BooleanField(default=False,help_text="disable notification if true")
-    icon  = models.ImageField(helper_text="Group Icon" blank=True,upload_to="icons")
-    date_created  = models.DateField(auto_now_add=True)
-    date_modified  = models.DateField(auto_now==True)
+    """ extend Group model to add extra info"""
+    description = models.TextField(blank=True, help_text="description of the group")
+    mute_notifications = models.BooleanField(default=False, help_text="disable notification if true")
+    icon = models.ImageField(help_text="Group icon", blank=True, upload_to="chartgroup")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
-        return reverse('chat:room',args=[str(self.id)])
-
+        from django.urls import reverse
+        return reverse('chat:room', args=[str(self.id)])
